0e514774e85b941cce1e561929b617d27be78038
retiredzombie <retiredzombie653@gmail.com>
2020-04-09 16:30:37 -0400

    Menu En Haut

--- WannabeFarmVille/Jeu.cs
+++ WannabeFarmVille/Jeu.cs
@@ -7,21 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestTilesetMario;
 
 namespace WannabeFarmVille
 {
     public partial class Jeu : Form
     {
-        TilesetImageGenerator TilesetIG;
 
         public Jeu()
         {
             InitializeComponent();
 
-            TilesetIG = new TilesetImageGenerator();
         }
 
         private void Jeu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void embaucherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
