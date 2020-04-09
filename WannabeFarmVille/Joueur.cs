2dc2f54da62ff4f4ae80ab122b03529248fdf9c3
Victor Gontar <61210146+GoldDiggerRu@users.noreply.github.com>
2020-04-09 16:37:38 -0400

    Ajout de classes
    Ajout de quelque classes et un liste de prénoms pour le générateur de noms.

---
+++ WannabeFarmVille/Joueur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WannabeFarmVille
+{
+    class Joueur
+    {
+        public Joueur()
+        {
+            Argent = 100;
+        }
+        public int Argent { get; set; }
+    }
+}
---
+++ WannabeFarmVille/Visiteur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WannabeFarmVille
+{
+    class Visiteur
+    {
+        public Visiteur(String nom)
+        {
+            Nom = nom;
+        }
+        public String Nom { get; set; }
+
+    }
+}
