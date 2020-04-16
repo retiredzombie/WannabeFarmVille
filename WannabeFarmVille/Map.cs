a0541ebcac4bccaff356aecaea5d8df1a011f835
retiredzombie <retiredzombie653@gmail.com>
2020-04-16 08:42:31 -0400

    Map

---
+++ WannabeFarmVille/Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WannabeFarmVille
+{
+    class Map
+    {
+        List<List<Tuile>> listeTuiles;
+        Map()
+        {
+            listeTuiles = new List<List<Tuile>>();
+        }
+
+        int getType(int x, int y)
+        {
+            return listeTuiles[y][x].GetType();
+        }
+    }
+}
---
+++ WannabeFarmVille/Tuile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WannabeFarmVille
+{
+    class Tuile
+    {
+        private int type;
+
+        public Tuile(int type)
+        {
+            this.type = type;
+        }
+
+       
+    }
+}
