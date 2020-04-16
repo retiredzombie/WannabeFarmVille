ff212e29c747bb2a4e4f2ee0808921ef0fb68231
retiredzombie <retiredzombie653@gmail.com>
2020-04-16 09:13:00 -0400

    Avancer Map

--- WannabeFarmVille/Tuile.cs
+++ WannabeFarmVille/Tuile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace WannabeFarmVille
     class Tuile
     {
         private int type;
+        Bitmap image;
 
         public Tuile(int type)
         {
